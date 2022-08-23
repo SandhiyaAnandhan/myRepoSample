678c51f9d31452dad91911258b2eb721bffe6768
SandhiyaAnandhan <111746178+SandhiyaAnandhan@users.noreply.github.com>
2022-08-23 18:01:37 +0530

    my name

---
+++ Myclass1.java
@@ -0,0 +1,31 @@
+package sandy2;
+
+public class Myclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	char a = '\u0053';
+	char b = '\u0041';
+	char c = '\u004e';
+	char d = '\u0044';
+	char e = '\u0048';
+	char f = '\u0049';
+	char g = '\u0059';
+	char i = '\u0041';
+	char A = '\u0073';
+	char B = '\u0061';
+	char C = '\u006e';
+	char D = '\u0064';
+	char E = '\u0068';
+	char F = '\u0069';
+	char G = '\u0079';
+	char I = '\u0061';
+			
+		
+			System.out.println("char1="+a+b+c+d+e+f+g+i);
+			System.out.println("char2="+A+B+C+D+E+F+G+I);
+		
+
+	}
+
+}
