9afb7b6956731ae3016168baf63d3487bbaf8af4
SandhiyaAnandhan <111746178+SandhiyaAnandhan@users.noreply.github.com>
2022-08-23 14:51:19 +0530

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,25 @@
+package mypkg;
+
+public class myclass {
+	public void myMethod() {
+		System.out.println("This is myMethod");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is main method");
+		
+		myclass m = new myclass();
+		m.myMethod();
+		
+
+	}
+	
+
+}
+
+
+	
+
+
